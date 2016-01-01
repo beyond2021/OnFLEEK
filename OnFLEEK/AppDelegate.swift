51bfff3db7360722557a0f6ba6f61099edbf9eb0
“KEEVIN <keevin.mitchell@gmail.com>
2016-01-01 11:43:29 -0500

    Created the camera and uploaded it successfully to iCloud.. rectified the image orientation issue

--- OnFLEEK/AppDelegate.swift
+++ OnFLEEK/AppDelegate.swift
@@ -40,6 +40,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    // MARK: UIApplicationDelegate
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        //TODO
+       // RemoteNotificationCondition.didFailToRegister(error)
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        //TODO
+       // RemoteNotificationCondition.didReceiveNotificationToken(deviceToken)
+    }
 
 
 }
--- OnFLEEK/FleekViewController.swift
+++ OnFLEEK/FleekViewController.swift
@@ -9,55 +9,77 @@
 import Foundation
 import UIKit
 import CloudKit
+import MobileCoreServices //kUTTypeMovie
 
 class FleekViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     // Set up the cloudKit manager
     let cloudKitManager = AAPLCloudManager()
     
-    // Set up the view for the fleek array
-    @IBOutlet weak var assetPreview: UIImageView!
-    
     // set up the name of the asset record
     var assetRecordName = " "
+    
+    //MARK: - Image
+    
+    var assetPreview =  UIImageView()
+    
+    @IBOutlet weak var assetPreviewContainer: UIView! {
+        didSet{
+            // When this is set I will put my image inside
+            assetPreviewContainer.addSubview(assetPreview)
+        }
+        
+    }
+    
+    
+    
+    func makeRoomForImage(){
+        var extraHeight: CGFloat = 0
+        if assetPreview.image?.aspectRatio > 0 {
+            if let width = assetPreview.superview?.frame.size.width {
+                let height = width / assetPreview.image!.aspectRatio
+                extraHeight = height - assetPreview.frame.height
+                assetPreview.frame = CGRect(x: 0, y: 0, width: width, height: height)
+            } else {
+                extraHeight = -assetPreview.frame.height
+                assetPreview.frame = CGRectZero
+            }
+            preferredContentSize = CGSize(width: preferredContentSize.width, height: preferredContentSize.height + extraHeight)
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
     
     // Do the upload to cloudKit
     @IBAction func uploadAction(sender: UIButton) {
         
-//        dispatch_async(dispatch_get_main_queue(), {
-//            let imagePicker: UIImagePickerController = UIImagePickerController();
-//            imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
-//            imagePicker.mediaTypes = [kUTTypeImage];
-//            imagePicker.allowsEditing = false;
-//            imagePicker.delegate = self;
-//            
-//            if(UIDevice.currentDevice().userInterfaceIdiom == .Pad){ // on a tablet, the image picker is supposed to be in a popover
-//                let popRect: CGRect = buttonRect;
-//                let popover: UIPopoverController = UIPopoverController(contentViewController: imagePicker);
-//                popover.presentPopoverFromRect(popRect, inView: self.view, permittedArrowDirections: UIPopoverArrowDirection.Up, animated: true);
-//            }else{
-//                self.presentViewController(imagePicker, animated: true, completion: nil);
-//            }
-//        });
-        
-        
-        
-        
-        
-        
-        
-        
-        let imagePicker = UIImagePickerController()
-        
-        // TODO GET FOR CAMERA AVAILABILITY
-        
-        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-        imagePicker.delegate = self
-        imagePicker.cameraCaptureMode = .Photo
-        
-        presentViewController(imagePicker, animated: true, completion: nil)
-        
+        // CREATE THE PICKER
+        // 1: IF I HAVE A PICKER
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            //SET MY PICKER'S SOURCETYPE TO BE THE CAMERA
+            let picker = UIImagePickerController()
+            picker.sourceType = .Camera
+            // IF I WAS DOING VIDEO I WOULD CHECK THE MEDIA TYPES HERE.
+            picker.mediaTypes = [kUTTypeImage as String]
+            //SET ITS DELEGATE
+            picker.delegate = self
+            // LETS ALLOW EDITING
+            picker.allowsEditing = true
+            
+            // PRESENT IT
+            presentViewController(picker, animated: true, completion: nil)
+            
+        }
     }
+    
     
     func noCamera(){
         let alertVC = UIAlertController(
@@ -73,79 +95,104 @@ class FleekViewController: UIViewController, UINavigationControllerDelegate, UII
             animated: true,
             completion: nil)
     }
-    // Picker delegate Method
+    
+    
+    //MARK:DELEGATE METHODS
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //THE IMAGE THE PICKED IS THE INFO OF THE DICTIONARY
+        var image = info[UIImagePickerControllerEditedImage] as? UIImage // we want it as a uiimage because info dic is anyObject.
+        
+        if image == nil {
+            //IF I COULDNT GET THE EDITED IMGAGE THEN I WILL TAKE THE  UNEDITED IMAGE
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+        
+        // NOW THAT I HAVE THE IMAGE I GOING TO SET MY IMAGEVIEW'S IMAGE TO THAT
+        //  imageView.image = image
+        //WE WILL THEN MAKE ROOM FOR IT
+        // makeRoomForImage()
+        // THEN WE ILL DISMISS
+        saveAndUploadImageToiCloud(image!)
+        
+        //     dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func saveAndUploadImageToiCloud(image:UIImage?){
+        
+        // LETS PUT THIS ON A GLOBAL QUEUE
+        
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { () -> Void in
-            // retrieve the image and resize it down
-            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-            var newSize = CGSizeMake(512, 512)
             
-            if (image.size.width > image.size.height) {
-                newSize.height = round(newSize.width * image.size.height / image.size.width);
-            } else {
-                newSize.width = round(newSize.height * image.size.width / image.size.height);
-            }
-            
-            // Size up the image
-            UIGraphicsBeginImageContext(newSize)
-            
-            image.drawAsPatternInRect(CGRectMake(0, 0, newSize.width, newSize.height))
-            
-            let data = UIImageJPEGRepresentation(UIGraphicsGetImageFromCurrentImageContext(), 0.75)
-            
-            UIGraphicsEndImageContext()
-            
-            
-            
-            do {
-            
-            // write the image out to a cache file 
-            let cachesDirectory = try NSFileManager.defaultManager().URLForDirectory(.CachesDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
-                
-                let temporaryName = NSUUID().UUIDString.stringByAppendingString(".jpeg")
-                
-                let writePath = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("jpeg")
-                
-                
-                let localURL = cachesDirectory.URLByAppendingPathComponent(temporaryName) as NSURL
-                
-                data?.writeToURL(localURL, atomically: true)
-                
-       
-                self.cloudKitManager.uploadAssetWithURL(localURL, completionHandler: { (record ) -> Void in
+            // IF WE CAN GET THAT IMAGE
+            if let image = image {
+                // THEN I WILL GET THE IMAGEDATA AS A JPEG
+                if let imageData = UIImageJPEGRepresentation(image, 1.0){
+                    // THEN WE WILL TURN THIS INTO NSDATA / OPPOSITE WOULD BE NSDATACONTENTSFORURL
+                    //1: GET THE FILE MANAGER
+                    //    let fileManager = NSFileManager.defaultManager() // WE ARE IN THE MAIN THREAD BUT JUST INCASE WE ARE NOT IN THE MAIN THREAD.
                     
-                    if ((record == nil)) {
-                       print("Handle this gracefully in your own app.")
+                    let fileManager = NSFileManager()
+                    //WHERE R WE GOING TO SAVE IT. LETS USE THE DOCUMENTS DIRECTORY
+                    if let docsDir = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! as? NSURL{
+                        //NOW WE HAVE THE DOCUMENTSDIRECTORY WE NEED A UNIQUE NAME FOR THE PHOTO WE TOOK
+                        let unique = NSDate.timeIntervalSinceReferenceDate() //UNIQUE BECAUSE U CANT TAKE 2 PHOTOS AT THE SAME INSTANCE OF TIME.
+                        //NEXT WE SET THE URL FOR MY PHOTO
+                        let url = docsDir.URLByAppendingPathComponent("\(unique)") //HERE WECREATE A STRING FROM THE UNIQUE TIME
                         
-                    } else {
-                       self.assetRecordName = record.recordID.recordName
-                        let alert = UIAlertController(title: "CloudKit Catalog", message: "Successfully Uploaded", preferredStyle: .Alert)
+                        //IF I CAN GET THE PATH TO THE URL
+                        if let path = (url.absoluteString) as? String {
+                            
+                            if imageData.writeToURL(url, atomically: true){
+                                // if I can seccessfully do this the I will upload it to icloud
+                                
+                                
+                                self.cloudKitManager.uploadAssetWithURL(url, completionHandler: { (record ) -> Void in
+                                    
+                                    if ((record == nil)) {
+                                        print("Handle this gracefully in your own app.")
+                                        
+                                    } else {
+                                        self.assetRecordName = record.recordID.recordName
+                                        let alert = UIAlertController(title: "Glamor Shot!!", message: "Successfully Uploaded", preferredStyle: .Alert)
+                                        
+                                        let action = UIAlertAction(title: "OK", style: .Default, handler: { (act) -> Void in
+                                            self.dismissViewControllerAnimated(true, completion: nil)
+                                        })
+                                        
+                                        alert.addAction(action)
+                                        self.presentViewController(alert, animated: true, completion: nil)
+                                        
+                                    }
+                                    
+                                })
+                                self.dismissViewControllerAnimated(true, completion: nil)
+                                
+                                
+                            }
+                            
+                            
+                        }
                         
-                        let action = UIAlertAction(title: "OK", style: .Default, handler: { (act) -> Void in
-                            self.dismissViewControllerAnimated(true, completion: nil)
-                        })
                         
-                        alert.addAction(action)
-                        self.presentViewController(alert, animated: true, completion: nil)
-                                         
                     }
                     
-               })
+                    
+                }
                 
-               self.dismissViewControllerAnimated(true, completion: nil)
                 
-                
-            } catch {
-                
-               print("There was a cashe error")
             }
             
-            
         }
+        
     }
+    
+    
+    
+    
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+        picker.delegate = nil
     }
     
     
@@ -167,21 +214,26 @@ class FleekViewController: UIViewController, UINavigationControllerDelegate, UII
             cloudKitManager.fetchRecordWithID(assetRecordName, completionHandler: { (record) -> Void in
                 let photoAsset = record[PhotoAssetField] as! CKAsset
                 
-                let image = UIImage(contentsOfFile: photoAsset.fileURL.path!)
+                if   let image = UIImage(contentsOfFile: photoAsset.fileURL.path!) {
+                    //  fixImageOrientation(image)
+                    
+                    self.assetPreview.image = image
+                    self.makeRoomForImage()
+                }
                 
-                self.assetPreview.image = image
-            
-            
             })
             
             
         }
-        
-        
-        
-        
-        
-        
-        
     }
 }
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width / size.height : 0
+        
+    }
+    
+}
+
+
